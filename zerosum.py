1e2e2d86d2e66924a8244924d09e040d71b1ece2
Zin4759 <vnaaiu7614@outlook.kr>
2023-06-15 01:56:20 +0900

    multiple inherite

--- zerosum.py
+++ zerosum.py
@@ -1,7 +1,25 @@
+class Game:
+    def __init__(self, user1, user2):
+        self.participant1 = user1
+        self.participant2 = user2
+        pass
+
+class Theory:
+    def __init__(self, author): # author = Author of the paper or theory
+        self.author = author
+        pass
+
 class GameTheory:
     def __init__(self, user1, user2):
         self.participant1 = user1
         self.participant2 = user2
+        pass
+
+class AdvancedGameTheory(Game, Theory):
+    def __init__(self, user1, user2, entire_pie, author):
+        Game.__init__(self, user1, user2)
+        self.entire_pie = entire_pie
+        Theory.__init__(self, author)
         pass
 
 class ZeroSum(GameTheory):
@@ -13,5 +31,9 @@ class ZeroSum(GameTheory):
 game = GameTheory("roller", "thyer")
 zero = ZeroSum("roller", "thyer", 80)
 
+adgame = AdvancedGameTheory("roller", "begger", 90, "Neumann")
+
 print(game.participant1, game.participant2)
 print(zero.participant1, zero.participant2, zero.entire_pie)
+print()
+print(adgame.participant1, adgame.participant2, adgame.entire_pie, adgame.author)
