4f04adbd36e68afa98ed095a15bd162681782b8f
Zin4759 <vnaaiu7614@outlook.kr>
2023-06-17 18:18:00 +0900

    plan todo

--- gametheory.py
+++ gametheory.py
@@ -1,10 +1,20 @@
+from resource_pie import *
+
 class Game:
+    # 게임 = 게임이론에서의 게임적인 부분을 담당할 예정
     def __init__(self, user1, user2):
         self._user1 = user1
         self._user2 = user2
         pass
+    def join(self, newbie):
+        # 임시로 만든 함수임.
+        # 아무래도 스택의 개념이 포함될 것 같다.
+        self._newbie = newbie
+        pass
 
 class Theory:
+    # 이론 = 게임이론에서의 룰을 담당할 예정? 
+    # 조만간 제외하거나 새로운 기능으로 변형할듯
     def __init__(self, author): # author = Author of the paper or theory
         self._author = author
         pass
@@ -12,8 +22,19 @@ class Theory:
 # 다중 상속을 이용하여 개념을 구현하려고 함.
 
 class GameTheory(Game, Theory):
-    def __init__(self, user1, user2, entire_pie, author):
+    # 이곳에 pie(resource) 클래스의 인스턴스를 매개변수로 주려고 함.
+    def __init__(self, user1, user2, pie_value, author):
         Game.__init__(self, user1, user2)
-        self._entire_pie = entire_pie
+        self._entire_pie = whole_resource(pie_value)
         Theory.__init__(self, author)
         pass
+    # 프로퍼티를 이용해서 self.entire_pie를 관리할 예정
+
+
+if __name__ == "__main__":
+    pie = whole_resource(50)
+    print(pie.resource)
+    pie.more_input = 30
+    print(pie.resource)
+
+    game = GameTheory("test1", "test2", 30, "von")
--- resource_pie.py
+++ resource_pie.py
@@ -1,4 +1,5 @@
 class wholeResource:
+    #Worst Sample
     def __init__(self, start_resource):
         self._resource = start_resource
         pass
@@ -8,14 +9,15 @@ class wholeResource:
         return self.start_resource
     
     @start_resource.setter
-    def add_resource(self, input_resource):
+    def more_input(self, input_resource):
         if input_resource >= 0:
             resource = resource + input_resource
             return resource
         else:
             raise ValueError("Input value cannot be nagative")
         
-class Resource:
+        
+class whole_resource:
     def __init__(self, resource):
         self._resource = resource
     
@@ -24,7 +26,7 @@ class Resource:
         return self._resource
     
     @resource.setter
-    def add_resource(self, input_more_value):
+    def more_input(self, input_more_value):
         if input_more_value < 0:
             raise ValueError("cannot input negative")
         self._resource = self._resource + input_more_value
@@ -33,12 +35,12 @@ class Resource:
 if __name__ == "__main__":
     # Worst Code
     # entire_pie = wholeResource(80)
-    # entire_pie.add_resource(30)
+    # entire_pie.more_input(30)
     # print(entire_pie.resource)
     # entire_pie.resource
 
     # Good Code
-    whole_resource = Resource(10)
-    print(whole_resource.resource)
-    whole_resource.add_resource = 24
-    print(whole_resource.resource)
+    entire_pie = whole_resource(10)
+    print(entire_pie.resource)
+    entire_pie.more_input = 24
+    print(entire_pie.resource)
