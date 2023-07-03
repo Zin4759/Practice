34f84c3d84871e1df21ca996d7ac9bdc84fdf3a5
Zin4759 <rlaxoan9327@gmail.com>
2023-07-03 13:02:17 +0900

    Edit Test

---
+++ Java/PracticeJava/src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    private String name;
+    public void meow() {
+        System.out.println(name + "~~~~~~"+"웁니다");
+    }
+    public Cat(String name) {
+        this.name = name;
+    }
+}
