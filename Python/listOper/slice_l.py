58ed741f13bafe7845dd283ee7f482cc8d2db096
Zin4759 <vnaaiu7614@outlook.kr>
2024-06-17 19:06:59 +0900

    Add files via upload

---
+++ Python/listOper/slice_l.py
@@ -0,0 +1,16 @@
+str1 = "This is Sentence!"
+print(str1[7:])
+print(str1[8:])
+print(str1[:5])
+
+List1 = ["Hello", "Goodbye", "See u tomorrow"]
+print(List1[:2])
+print(List1[:1])
+
+List2 = [4, 1, '5', 2, 3, '6', 9, 8, 7, 10]  # 5~6
+print(List2[2:6])
+
+List3 = [1, '2', 3, 4, 5, 6, 7, 8, '9', 10]  # 2~9
+print(List3[1:9])
+
+
