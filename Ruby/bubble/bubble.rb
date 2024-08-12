4c8179c8daf842b69217c26e7401e510e0e81576
Zin4759 <vnaaiu7614@outlook.kr>
2024-08-12 20:12:32 +0900

    Add files via upload

--- Ruby/bubble/bubble.rb
+++ Ruby/bubble/bubble.rb
@@ -9,12 +9,6 @@ class Bubble
     @data = data["2nd"]
   end
 
-  def swap(f_index, s_index)
-    temp = @data[f_index]
-    @data[f_index] = @data[s_index]
-    @data[s_index] = temp
-  end
-
   def sort
     n = @data.length
     i = 0
@@ -22,7 +16,7 @@ class Bubble
       j = 0
       while j < n-i-1
         if @data[j]>@data[j+1]
-          swap(j, j+1)
+          @data[j], @data[j+1] = @data[j+1], @data[j]
         end
         j += 1
       end
